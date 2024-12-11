37913f09b0176014bc0c9cb8a1d6434ed97876a0
donny1441 <deacosta101@student.hbuhsd.edu>
2024-12-11 21:50:31 +0000

    studied cout

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,17 @@
 
 int main()
 {
-  // write code here
-  
+  // SET age TO 10
+  int age = 10;
+  double price = 19.99;
+  bool hasPets = true;
+
+// \n is a new line
+// std::endl is a new line
+// std::cout << computer output
+  std::cout << age << "\n" << price << std::endl << hasPets << std::endl;
+
+  std::cout << "I am " << age << " years old.\n";
+
   return 0;
 }
